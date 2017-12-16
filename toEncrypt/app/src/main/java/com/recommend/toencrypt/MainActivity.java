67687012a095f18b703a9df3a71dcd77e3fd05ed
luowp <261000975@163.com>
2017-12-16 17:04:21 +0800

    toEncrypt测试工程

---
+++ toEncrypt/app/src/main/java/com/recommend/toencrypt/MainActivity.java
@@ -0,0 +1,17 @@
+package com.recommend.toencrypt;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by luowp on 2017/12/16.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
