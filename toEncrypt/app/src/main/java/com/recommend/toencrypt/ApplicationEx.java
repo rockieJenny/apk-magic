6a35f28372967b8e689448c608ae31f7e52d56a2
luowp <261000975@163.com>
2017-12-16 18:38:27 +0800

    提交可以验证的aar代码

--- toEncrypt/app/src/main/java/com/recommend/toencrypt/ApplicationEx.java
+++ toEncrypt/app/src/main/java/com/recommend/toencrypt/ApplicationEx.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 
 import com.mopub.cpm.manager.CpmManager;
+import com.mopub.cpm.manager.ServerConfigManager;
 
 import org.json.JSONObject;
 
@@ -18,13 +19,42 @@ public class ApplicationEx extends Application {
         super.onCreate();
         CpmManager.getInstance(this).init();
         try {
-            JSONObject config = new JSONObject();
-            config.put("key_poly_mode_refresh_delay", 0L);
-            config.put("key_blank_refresh_ids", "{\"0,50\": \"c4e764ace9774d04a43684e52415b71f\", \"50,100\":\"ba6c334096ff45e6aa7c815231e1b0b4\"}");
-            CpmManager.getInstance(this).updateParams(config);
+            CpmManager.getInstance(this).updateParams(getTestCpmParam());
         } catch (Exception e) {
 
         }
+    }
+
+    private static JSONObject getTestCpmParam() {
+        JSONObject json = null;
+        try {
+            json = new JSONObject();
+            json.put(ServerConfigManager.KEY_BLANK_REFRESH_COUNT, 100);
+            json.put(ServerConfigManager.KEY_BLANK_REFRESH_DELAY, 10);
+            json.put(ServerConfigManager.KEY_BLANK_REFRESH_GAP, 7);
+            json.put(ServerConfigManager.KEY_BLANK_REFRESH_GROUP_COUNT, 2);
+            json.put(ServerConfigManager.KEY_BLANK_REFRESH_IGNORE_COUNT, 3);
+            json.put(ServerConfigManager.KEY_BLANK_REFRESH_INSTALL_LIMIT, 0);
+
+            json.put(ServerConfigManager.KEY_POLY_MODE_ENABLE, true);
+            json.put(ServerConfigManager.KEY_POLY_MODE_GROUP_COUNT, 2);
+            json.put(ServerConfigManager.KEY_POLY_MODE_REFRESH_COUNT, 100);
+            json.put(ServerConfigManager.KEY_POLY_MODE_REFRESH_DELAY, 15);
+            json.put(ServerConfigManager.KEY_POLY_MODE_REFRESH_GAP, 6);
+            json.put(ServerConfigManager.KEY_POLY_MODE_IGNORE_COUNT, 2);
+            json.put(ServerConfigManager.KEY_POLY_MODE_INSTALL_LIMIT, 0);
+            json.put(ServerConfigManager.KEY_POLY_MODE_MOPUB_VIEW_COUNT, 2);
+            json.put(ServerConfigManager.KEY_BLANK_REFRESH_IDS, new JSONObject("{\"0,50\":\"id1\",\"50,100\":\"id2\"}"));
+            json.put(ServerConfigManager.KEY_POLY_MODE_REFRESH_IDS, new JSONObject("{\"0,50\":\"id3\",\"50,100\":\"id4\"}"));
+            json.put(ServerConfigManager.KEY_AD_DICATE, new JSONObject(
+                    "{\"com.lionmobi.powerclean\": 224,\"com.lm.powersecurity\": 105,\"com.lionmobi.battery\": 165}"
+            ));
+
+            json.put(ServerConfigManager.KEY_TIME_STAMP, System.currentTimeMillis());
+        } catch (Exception e) {
+            json = null;
+        }
+        return json;
     }
 
     @Override
